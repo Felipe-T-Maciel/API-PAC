3e993ff4a570b4d00f8b5e9a2d02853cdd42ae06
Felipe Tomio Maciel <felipetomio@weg.net>
2025-05-19 09:57:22 -0300

    save

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.config.configuration import configure_all
 from app.controller.user_controller import userController
+import uvicorn
 
 app = FastAPI()
 configure_all()
 
 app.include_router(router=userController)
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8080, reload=True, log_level="debug",
+                workers=1, limit_concurrency=1, limit_max_requests=1)
